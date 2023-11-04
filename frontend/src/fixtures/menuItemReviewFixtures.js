5b519a54a9f6f3ab0aa7d2fd198c8208c37f515f
bella-ward <108088761+bella-ward@users.noreply.github.com>
2023-11-03 22:06:58 -0700

    bw - added fixtures for MenuItemReview

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,41 @@
+const menuItemReviewFixtures = {
+    oneItemReview: {
+        "id": 1, 
+        "itemId": 14,
+        "reviewerEmail": "ward@ucsb.edu",
+        "stars": 5,
+        "dateReviewed": "2022-01-02T12:00:00",
+        "comments": "I love the chocolate cake!"
+
+    },
+    threeDates: [
+        {
+            "id": 1, 
+            "itemId": 14,
+            "reviewerEmail": "ward@ucsb.edu",
+            "stars": 5,
+            "dateReviewed": "2022-01-02T12:00:00",
+            "comments": "I love the chocolate cake!"
+
+        },
+        {
+            "id": 2, 
+            "itemId": 24,
+            "reviewerEmail": "student_email@ucsb.edu",
+            "stars": 3,
+            "dateReviewed": "2022-12-18T12:00:00",
+            "comments": "The noodles are okay."
+        },
+        {
+            "id": 3, 
+            "itemId": 12,
+            "reviewerEmail": "jelly@yahoo.com",
+            "stars": 1,
+            "dateReviewed": "2020-08-08T08:00:00",
+            "comments": "Pho is my favorite!"
+        }
+    ]
+};
+
+
+export { menuItemReviewFixtures };
