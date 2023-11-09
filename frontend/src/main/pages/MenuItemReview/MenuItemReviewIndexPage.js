4a813c2b87ba82b84b06d83dbd2eeef643a4ad6e
bella-ward <108088761+bella-ward@users.noreply.github.com>
2023-11-09 01:36:26 -0800

    bw - changed index url :/

--- frontend/src/main/pages/MenuItemReview/MenuItemReviewIndexPage.js
+++ frontend/src/main/pages/MenuItemReview/MenuItemReviewIndexPage.js
@@ -7,8 +7,8 @@ export default function MenuItemReviewIndexPage() {
     <BasicLayout>
       <div className="pt-2">
         <h1>Index page not yet implemented</h1>
-        <p><a href="/placeholder/create">Create</a></p>
-        <p><a href="/placeholder/edit/1">Edit</a></p>
+        <p><a href="/menuitemreview/create">Create</a></p>
+        <p><a href="/menuitemreview/edit/1">Edit</a></p>
       </div>
     </BasicLayout>
   )
