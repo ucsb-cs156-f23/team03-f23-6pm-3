f8b9b28129e2153a88562ee09335f2763ddd804f
Salvador Gracia <salvadorgracia@ucsb.edu>
2023-11-12 13:51:16 -0800

    sg - addded edit page and tests

---
+++ frontend/src/stories/pages/RecommendationRequest/RecommendationRequestEditPage.stories.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import { recommendationRequestFixtures } from 'fixtures/recommendationRequestFixtures';
+import { rest } from "msw";
+
+import RecommendationRequestEditPage from 'main/pages/RecommendationRequest/RecommendationRequestEditPage';
+
+export default {
+    title: 'pages/RecommendationRequest/RecommendationRequestEditPage',
+    component: RecommendationRequestEditPage
+};
+
+const Template = () => <RecommendationRequestEditPage storybook={true}/>;
+
+export const Default = Template.bind({});
+Default.parameters = {
+    msw: [
+        rest.get('/api/currentUser', (_req, res, ctx) => {
+            return res( ctx.json(apiCurrentUserFixtures.userOnly));
+        }),
+        rest.get('/api/systemInfo', (_req, res, ctx) => {
+            return res(ctx.json(systemInfoFixtures.showingNeither));
+        }),
+        rest.get('/api/ucsbdates', (_req, res, ctx) => {
+            return res(ctx.json(recommendationRequestFixtures.threeRecomendation[0]));
+        }),
+        rest.put('/api/ucsbdates', async (req, res, ctx) => {
+            var reqBody = await req.text();
+            window.alert("PUT: " + req.url + " and body: " + reqBody);
+            return res(ctx.status(200),ctx.json({}));
+        }),
+    ],
+}
