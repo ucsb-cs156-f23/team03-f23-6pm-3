126c189052c28d5b23f318a9bc7781e0b89f3ee5
HououinKyouma-2036 <lixing_guo@ucsb.edu>
2023-11-12 18:55:49 -0800

    add create Page; 100% in mutation test

--- frontend/src/stories/components/HelpRequest/HelpRequestForm.stories.js
+++ frontend/src/stories/components/HelpRequest/HelpRequestForm.stories.js
@@ -3,7 +3,7 @@ import HelpRequestForm from "main/components/HelpRequest/HelpRequestForm"
 import { helpRequestFixtures } from 'fixtures/helpRequestFixtures';
 
 export default {
-    title: 'components/HelpRequests/HelpRequestForm',
+    title: 'components/HelpRequest/HelpRequestForm',
     component: HelpRequestForm
 };
 
