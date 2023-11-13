a061e4e055c0d06aa12429b22d991e126b487b41
HououinKyouma-2036 <lixing_guo@ucsb.edu>
2023-11-12 17:38:15 -0800

    Fix App.js

--- frontend/src/tests/utils/HelpRequestUtils.test.js
+++ frontend/src/tests/utils/HelpRequestUtils.test.js
@@ -43,7 +43,7 @@ describe("HelpRequestUtils", () => {
 
             // assert
             expect(result).toEqual({
-                url: "/api/helprequests",
+                url: "/api/helprequest",
                 method: "DELETE",
                 params: { id: 17 }
             });
